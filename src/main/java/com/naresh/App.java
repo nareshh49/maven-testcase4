f13bf40e79ff33ba9a8706b15f1542e89d72500e
nareshh49 <124552811+nareshh49@users.noreply.github.com>
2023-08-04 16:14:36 +0530

    Update App.java

--- src/main/java/com/naresh/App.java
+++ src/main/java/com/naresh/App.java
@@ -5,6 +5,6 @@ package com.naresh;
 public class App {
     public static void main(String[] args) {
         
-        System.out.printf("Hellowrold/n");
+        System.out.printf("Hello wrold ");
     }
 }
